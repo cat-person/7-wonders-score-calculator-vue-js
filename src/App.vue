4b57a338d6d7077c2bd75d9a1e4a55f2e1b92419
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-11-24 20:53:37 +0400

    Shown result table

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
   // import { createApp } from 'vue'
   // import { RouterLink, RouterView } from 'vue-router'
-  // import ResultTable from './views/ResultTable.vue';
+  import ResultTable from '@/views/ResultTable.vue';
 </script> 
 
 
 
 <template>
-    <!-- <ResultTable class="wrapper"/> -->
-
-    <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+    <ResultTable/>
 </template>
 
 <style scoped>
--- src/views/ResultTable.vue
+++ src/views/ResultTable.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <!-- <div> -->
     <h1> AAAAAAAAAAAAAAAAA </h1>
-    <table>
+    <!-- <table>
       <thead>
         <tr>
           <th v-for="header in headers" :key="header">{{ header }}</th>
@@ -12,21 +12,27 @@
           <td v-for="cell in row" :key="cell">{{ cell }}</td>
         </tr>
       </tbody>
-    </table>
-  </div>
+    </table> -->
+  <!-- </div> -->
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      headers: ['Name', 'Age', 'Occupation'],
-      data: [
-        ['John Doe', 30, 'Developer'],
-        ['Jane Smith', 25, 'Designer'],
-        ['Bob Johnson', 40, 'Manager']
-      ]
-    }
+  // import myComponent from 'my-component'
+  name: 'ResultTable',
+  data () {
+    return {}
   }
+
+  // data() {
+  //   return {
+  //     headers: ['Name', 'Age', 'Occupation'],
+  //     data: [
+  //       ['John Doe', 30, 'Developer'],
+  //       ['Jane Smith', 25, 'Designer'],
+  //       ['Bob Johnson', 40, 'Manager']
+  //     ]
+  //   }
+  // }
 }
 </script>
