97644712f02d818d9ba0546b7deedba93efca9b4
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-01-09 21:30:24 +0400

    Added military points counter to each epoch

--- src/routes/AddPlayer/components/Military.vue
+++ src/routes/AddPlayer/components/Military.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="military">
         <h3>Military</h3>
-        <div>
-            <button @click="handleClick('bronze', 'left')">{{ this.battles['bronze.left'] }}</button>
-            Bronze
-            <button @click="handleClick('bronze', 'right')">{{ this.battles['bronze.right'] }}</button>
+        <div class="epoch_container">
+            <button class="battle_button" @click="handleClick('bronze', 'left')">{{ this.battles['bronze.left'] }}</button>
+            <p class="epoch"> Bronze: {{ calcBronze(battles['bronze.left'], battles['bronze.right']) }} </p>
+            <button class="battle_button" @click="handleClick('bronze', 'right')">{{ this.battles['bronze.right'] }}</button>
         </div>
-        <div>
-            <button @click="handleClick('silver', 'left')">{{ this.battles['silver.left'] }}</button>
-            Silver
-            <button @click="handleClick('silver', 'right')">{{ this.battles['silver.right'] }}</button>
+        <div class="epoch_container">
+            <button class="battle_button" @click="handleClick('silver', 'left')">{{ this.battles['silver.left'] }}</button>
+            <p class="epoch"> Silver: {{ calcSilver(battles['silver.left'], battles['silver.right']) }} </p>
+            <button class="battle_button" @click="handleClick('silver', 'right')">{{ this.battles['silver.right'] }}</button>
         </div>
-        <div>
-            <button @click="handleClick('golden', 'left')">{{ this.battles['golden.left'] }}</button>
-            Gold
-            <button @click="handleClick('golden', 'right')">{{ this.battles['golden.right'] }}</button>
+        <div class="epoch_container">
+            <button class="battle_button" @click="handleClick('golden', 'left')">{{ this.battles['golden.left'] }}</button>
+            <p class="epoch"> Gold: {{ calcGold(battles['golden.left'], battles['golden.right']) }} </p>
+            <button class="battle_button" @click="handleClick('golden', 'right')">{{ this.battles['golden.right'] }}</button>
         </div>
-        <p>Military points: {{ calcMilitary(this.battles) }}</p>
+        <p>Total ilitary points: {{ calcMilitary(this.battles) }}</p>
     </div>
 
 </template>
@@ -29,33 +29,32 @@ export default {
     data() {
         console.error(`Military.battles: ${JSON.stringify(this.battles)}`)
         return {
-        battles: this.battles
+            battles: this.battles
         }
     },
     methods: {
+        checkVictory(battle) {
+            if(battle == 'Won') {
+                return 1
+            }
+            return 0
+        },
+        calcBronze(left, right) {
+            return 2 * (this.checkVictory(left) + this.checkVictory(right) - 1)
+        },
+        calcSilver(left, right) {
+            return 3 * (this.checkVictory(left) + this.checkVictory(right)) - 2
+        },
+        calcGold(left, right) {
+            return 5 * (this.checkVictory(left) + this.checkVictory(right)) - 2
+        },
+        
+        
+
         calcMilitary(battles) {
-            var result = -6
-
-            if(battles['bronze.left'] === 'Won') {
-                result += 2
-            }
-            if(battles['bronze.right'] === 'Won') {
-                result += 2
-            }
-            if(battles['silver.left'] === 'Won') {
-                result += 4
-            }
-            if(battles['silver.right'] === 'Won') {
-                result += 4
-            }
-            if(battles['golden.left'] === 'Won') {
-                result += 6
-            }
-            if(battles['golden.right'] === 'Won') {
-                result += 6
-            }
-
-            return result
+            return this.calcBronze(battles['bronze.left'], battles['bronze.right'])
+                + this.calcSilver(battles['silver.left'], battles['silver.right'])
+                + this.calcGold(battles['golden.left'], battles['golden.right'])
         },
 
         handleClick(age, neighbour){
@@ -74,5 +73,23 @@ export default {
     padding-bottom: 4mm;
     padding-top: 1mm;
     margin-bottom: 3mm;
+}
+.epoch_container {
+    height: 6mm;
+    display: flex;
+    flex-direction: row;
+    justify-items: center;
+    justify-content: center;
+}
+.battle_button {
+    margin: 0;
+    height: 6mm;
+    width: 12mm;
+}
+.epoch {
+    margin: 0;
+    height: 6mm;
+    width: 40mm;
+    text-align: center;
 }
 </style>
