2f4cbd9e849e1b74789d412e0f2296199680d2ec
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-01-13 17:32:46 +0400

    Experimenting with score issue by deepcloning scoreData instead of using the ref

--- src/routes/Results/Results.vue
+++ src/routes/Results/Results.vue
@@ -21,13 +21,13 @@
   import wonders from '@/assets/wonders.json'
   import ResultItem  from './components/ResultItem.vue';
   import TopBar from '../Common/components/TopBar.vue';
+  import { toRaw } from 'vue';
   import * as util from '@/utils/calc';
 
   export default {
     data() {
       console.error(`Players.data(): { playerScores: ${JSON.stringify(this.playerScores)} }`)
       return {
-        playerScores: this.playerScores,
         wonders: wonders,
       };
     },
@@ -47,9 +47,8 @@
 
         for (let scoreIdx = 0; scoreIdx < playerScores.length; scoreIdx++) {
           let currentIdx = scoreIdx
-          let currentScore = playerScores[currentIdx]
+          let currentScore = structuredClone(toRaw(playerScores[currentIdx]))
           currentScore.finalPoints = util.calcSum(currentScore)
-          currentScore.rank = scoreIdx + 1
           
           while (0 < currentIdx) { 
             let scoreToCompare = result[currentIdx-1]
@@ -57,21 +56,8 @@
               || (scoreToCompare.finalPoints == currentScore.finalPoints 
                 && scoreToCompare.coinCount < scoreToCompare.coinCount
               )) {
-                if(scoreToCompare.rank < currentScore.rank) {
-                  let savedRank = currentScore.rank
-                  currentScore.rank = scoreToCompare.rank
-                  scoreToCompare.rank = savedRank
-                } else if(scoreToCompare.rank == currentScore.rank) {
-                  scoreToCompare.rank = result[currentIdx + 1].rank 
-                }
                 result[currentIdx] = scoreToCompare
                 currentIdx--
-            } else {
-              if(scoreToCompare.finalPoints == currentScore.finalPoints 
-                && scoreToCompare.coinCount == scoreToCompare.coinCount){
-                currentScore.rank = scoreToCompare.rank
-              }
-              break;
             }
           }
           result[currentIdx] = currentScore
