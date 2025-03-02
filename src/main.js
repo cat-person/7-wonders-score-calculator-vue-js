ed780de423311832291365b8cc0cc8883419c430
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-03-03 02:47:01 +0400

    Used router for the app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
 
 import App from './App.vue'
+import StartGame from './routes/StartGame/StartGame.vue'
+import Players from './routes/Players/Players.vue'
+import AddPlayer from './routes/AddPlayer/AddPlayer.vue'
+import EditPlayer from './routes/EditPlayer/EditPlayer.vue'
+import Results from './routes/Results/Results.vue'
+
+const routes = [
+  { path: '/', component: StartGame },
+  { path: '/:session_id', component: Players },
+  { path: '/:session_id/add_player', component: AddPlayer },
+  { path: '/:session_id/edit_player', component: EditPlayer },
+  { path: '/:session_id/results', component: Results },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
 
 createApp(App)
+    .use(router)
     .mount('#app')
