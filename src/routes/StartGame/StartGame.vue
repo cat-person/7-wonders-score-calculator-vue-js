cda5616163a4fc8d1e73d6a3a158f25dd079de63
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-03-10 20:20:10 +0400

    Removed link copying

--- src/routes/StartGame/StartGame.vue
+++ src/routes/StartGame/StartGame.vue
@@ -35,12 +35,12 @@
     },
     methods: {
       handleStartNewGame(sessionId){
-        console.log(`handleStartNewGame ${sessionId}`)
-        const route = this.$router.resolve(`/${sessionId}/`);
-        const absoluteURL = new URL(route.href, window.location.origin).href;
-        window.navigator.clipboard.writeText(absoluteURL)
-        this.$toast('Link copied to clipboard')
-        this.$toast(absoluteURL);
+        // console.log(`handleStartNewGame ${sessionId}`)
+        // const route = this.$router.resolve(`/${sessionId}/`);
+        // const absoluteURL = new URL(route.href, window.location.origin).href;
+        // window.navigator.clipboard.writeText(absoluteURL)
+        // this.$toast('Link copied to clipboard')
+        // this.$toast(absoluteURL);
 
         this.$router.push(`/${sessionId}/add`)
       }
