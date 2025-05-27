8e5487c9a1085d9470b0be520a8307767336205b
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-05-27 23:25:47 +0400

    Fixed TopBar alignment

--- src/routes/Common/components/TopBar.vue
+++ src/routes/Common/components/TopBar.vue
@@ -1,49 +1,55 @@
 <script>
-  export default {
+export default {
     props: {
-      showClose: Boolean,
-      title: String
+        showClose: Boolean,
+        title: String,
     },
     data() {
-      return {
-        currentName: this.name
-      }
+        return {
+            currentName: this.name,
+        };
     },
     methods: {
-      handleCloseClicked(){
-        this.$router.back()
-      }
-    }
-  }
+        handleCloseClicked() {
+            this.$router.back();
+        },
+    },
+};
 </script>
 
 <template>
     <div class="horizontal">
-      <img v-if="showClose" class="button" src="@/assets/icon_back.svg" @click="handleCloseClicked"/>
-      <h3 class="title">{{ this.title }}</h3>
+        <img
+            v-if="showClose"
+            class="button"
+            src="@/assets/icon_back.svg"
+            @click="handleCloseClicked"
+        />
+        <h3 class="title">{{ this.title }}</h3>
     </div>
 </template>
 
 <style scoped>
-  .horizontal {
+.horizontal {
     position: relative;
     height: 12mm;
     width: 96%;
     margin-bottom: 2mm;
     justify-self: center;
     justify-content: center;
-  }
-  .title {
+}
+.title {
     margin: 0mm;
-    height: 8mm;
+    height: 12mm;
     align-content: center;
-  }
-  .button {
+    vertical-align: center;
+}
+.button {
     justify-self: left;
     position: absolute;
     left: 0mm;
-    margin: 0mm;
+    margin: 2mm;
     width: 8mm;
     height: 8mm;
-  }
+}
 </style>
