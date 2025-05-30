0880d3c2e4393d0df7e75841db3601830e47b50b
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-05-30 13:23:48 +0400

    Cleaned up libs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +9,11 @@ export default defineConfig({
   base: "./",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   build: {
-    outDir: 'docs'
+    outDir: "docs",
   },
-  server: { port: 3000 }
-})
+  server: { port: 3000 },
+});
