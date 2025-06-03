cb52ca9a634eee5f4f75ea844b292d7ca850701b
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2025-06-04 01:52:21 +0400

    Lame change language impl

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
     <main class="root">
         <RouterView />
+        <div style="flex-grow: 1" />
+        <button class="change-language" @click="handleChangeLanguage">
+            Change Language ({{ $i18n.locale }})
+        </button>
     </main>
 </template>
 
 <script>
 export default {
     name: "App",
+    methods: {
+        handleChangeLanguage() {
+            if (this.$i18n.locale === "en") {
+                this.$i18n.locale = "ru";
+            } else {
+                this.$i18n.locale = "en";
+            }
+        },
+    },
 };
 </script>
 
@@ -21,13 +34,24 @@ export default {
 }
 
 .root {
+    display: flex;
+    flex-direction: column;
     width: 100%;
+    min-height: 100%;
     margin: 0mm;
     padding: 0mm;
     padding-top: 3mm;
-    padding-bottom: 3mm;
     max-width: 160mm;
     justify-self: center;
+    justify-content: center;
     background-color: aliceblue;
+}
+
+.change-language {
+    align-self: center;
+    width: 60mm;
+    margin: 0mm;
+    margin-top: 24mm;
+    margin-bottom: 3mm;
 }
 </style>
